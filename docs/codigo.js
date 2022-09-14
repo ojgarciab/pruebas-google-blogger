28cb0a82ab7816a84bfb69426c7e312160bcfd95
Óscar García <ojgarciab@users.noreply.github.com>
2022-09-15 00:04:39 +0200

    Terminado el flujo

--- docs/codigo.js
+++ docs/codigo.js
@@ -2,25 +2,29 @@ const authUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 const clientId = "507875559835-l3sdlaagl3fedbqvk22jt6ck2aeiaq3m.apps.googleusercontent.com";
 const redirect = "https://ojgarciab.github.io/pruebas-google-blogger/";
 const scope = "https://www.googleapis.com/auth/blogger";
-const aleatorio = Math.random(0).toString(36).substr(2);
 
 /* null si no tenemos aún el testigo almacenado */
-let access_token = sessionStorage.getItem('access_token');
+let parametros = JSON.parse(sessionStorage.getItem('parametros'));
 
 if (document.location.hash !== "") {
-    const token_obtenido = document.location.hash.match(/\#(?:access_token)\=([\S\s]*?)\&/);
-    console.log("token_obtenido", token_obtenido);
+    /* Obtenemos todos los parámetros recibidos en el flujo OAuth */
+    const parametros = Object.fromEntries(
+        URLSearchParams(document.location.hash.substr(1))
+    );
+    console.log("parametros", parametros);
     /* Actualizamos el testigo de sesión */
-    if (token_obtenido.length === 2) {
-        access_token = token_obtenido[1];
-        sessionStorage.setItem('access_token', access_token);
+    if (parametros.hasOwnProperty("access_token") === true) {
+        console.log("state", sessionStorage.setItem('aleatorio'), parametros.state)
+        sessionStorage.setItem('parametros', JSON.stringify(parametros));
         boton_login.style.display = "none";
     }
 }
 
-console.log("access_token", access_token);
-if (access_token === null) {
+console.log("access_token", parametros.access_token);
+if (parametros.access_token === undefined) {
     addEventListener('DOMContentLoaded', (event) => {
+        /* Generamos un valor aleatorio que deberá ser devuelto igual */
+        sessionStorage.setItem('aleatorio', Math.random(0).toString(36).substr(2));
         boton_login.addEventListener('click', () => {
             let url = new URL(authUrl);
             url.searchParams.append("client_id", clientId);
